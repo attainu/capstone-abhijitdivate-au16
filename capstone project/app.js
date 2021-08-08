d16db28dd2c7f4c3573cf627764616120cb40bc6
satender <satenderkumarparihar@gmail.com>
2021-08-08 16:06:17 +0530

    DB connected

---
+++ capstone project/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    useCreateIndex: true
+    })
+    .then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`);
+});
+
+const postRoutes = require('./routes/post');
+
+app.use(morgan('dev'));
+app.use('/', postRoutes);
+
+
+
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`A Node Js API is listening on port: ${port}`);
+});
