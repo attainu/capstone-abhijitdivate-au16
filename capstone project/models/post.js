e34fa51daf5cf2a1f2c4815fe668d3db1adde2f1
satender <satenderkumarparihar@gmail.com>
2021-08-15 12:55:24 +0530

    some changes in routes

--- capstone project/models/post.js
+++ capstone project/models/post.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
 
 const postSchema = new mongoose.Schema({
     title: {
@@ -13,12 +14,23 @@ const postSchema = new mongoose.Schema({
         data: Buffer,
         contenType: String
     },
-    
+    postedBy: {
+        type: ObjectId,
+        ref: 'User'
+    },
     created: {
         type: Date,
         default: Date.now
     },
-    
+    // updated: Date,
+    // likes: [{ type: ObjectId, ref: 'User' }],
+    // comments: [
+    //     {
+    //         text: String,
+    //         created: { type: Date, default: Date.now },
+    //         postedBy: { type: ObjectId, ref: 'User' }
+    //     }
+    // ]
 });
 
 module.exports = mongoose.model('Post', postSchema);
