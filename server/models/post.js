aef646d7eaec79f8c0cefe1a52305f35c6d41661
satender kumar <satenderkumarparihar@gmail.com>
2021-08-30 23:41:50 +0530

    comments and like schema updated

--- server/models/post.js
+++ server/models/post.js
@@ -13,6 +13,13 @@ const postSchema = new mongoose.Schema({
         type:String,
         required:true
     },
+
+    comments:[{
+        text:String,
+        name:String,
+        postedBy:{type:ObjectId,ref:"User"}
+    }],
+    likes:[{type:ObjectId,ref:"User"}],
     postedby:{
         type:ObjectId,
         ref:"User"
