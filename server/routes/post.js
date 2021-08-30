1f2059f05f73ce7ed0a0e07bfbde70a7e0239e46
satender kumar <satenderkumarparihar@gmail.com>
2021-08-30 23:38:26 +0530

    delete comments and like routes created

--- server/routes/post.js
+++ server/routes/post.js
@@ -5,9 +5,10 @@ const requireLogin = require("../middleware/requireLogin");
 
 const Post = mongoose.model("Post")
 
-router.get("/allpost",(req,res)=>{
+router.get("/allpost",requireLogin,(req,res)=>{
     Post.find()
     .populate("postedby","_id name")
+    .populate("comments.postedBy","_id name")
     .then(posts=>{
         res.json(posts)
     }).catch(err=>{
@@ -35,13 +36,80 @@ router.post("/createpost" ,requireLogin,(req,res)=>{
     })
 })
 router.get("/mypost",requireLogin,(req,res)=>{
-    Post.find({postedby:req.user_id})
+    Post.find({postedby:req.user._id})
     .populate("postedby","_id name")
     .then(mypost=>{
         res.json({mypost})
     })
     .catch(err=>{
         console.log(err);
+    })
+})
+router.put("/like",requireLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{likes:req.user._id}
+    },{
+        new:true
+    }).exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+router.put("/unlike",requireLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $pull:{likes:req.user._id}
+    },{
+        new:true
+    }).exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+router.put("/comment",requireLogin,(req,res)=>{
+    const comment= {
+        text:req.body.text,
+        name:req.body.name,
+        postedBy:req.user
+    }
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{comments:comment}
+    },{
+        new:true
+    })
+    .populate("postedBy","_id name")
+    .populate("comments.postedBy","_id name")
+    .exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+router.delete("/deletepost/:postId",requireLogin,(req,res)=>{
+    Post.findOne({_id:req.params.postId})
+    .populate("postedby","_id")
+    .exec((err,post)=>{
+        if(err||!post){
+            return res.status(422).json({error:err})
+        }
+        if(post.postedby._id.toString()===req.user._id.toString()){
+            post.remove()
+            .then(result=>{
+                res.json(result)
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+        }
     })
 })
 module.exports = router
