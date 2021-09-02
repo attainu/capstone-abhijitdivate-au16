d795000de65808289b1be511c7ab4c5da0605fc2
satender kumar <satenderkumarparihar@gmail.com>
2021-09-02 10:51:35 +0530

    USer model updates

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const{ObjectId} = mongoose.Schema.Types
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -13,7 +14,14 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    pic:{
+        type:String,
+        default:"https://res.cloudinary.com/dfvaav8sj/image/upload/v1630390403/blank-profile-pic_z2lr9d.png"
+    },
+    followers:[{type:ObjectId,ref:"User"}],
+    following:[{type:ObjectId,ref:"User"}]
+
 })    
 
 module.exports = mongoose.model("User", userSchema); 
