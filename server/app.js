ac38a0d00b81ea03cd040917c32b82820e49c622
satender kumar <satenderkumarparihar@gmail.com>
2021-09-02 10:55:36 +0530

    App.js server updated

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 const express  = require("express");
 const app = express()
 const mongoose = require("mongoose");
-
+const {MONGO_URI} = require("./valuekeys.js");
 const PORT = 5000
-const dotenv = require('dotenv');
-dotenv.config()
+// const dotenv = require('dotenv');
+// dotenv.config()
 
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 
@@ -21,8 +21,9 @@ console.log(`DB connection error: ${err.message}`);
 require("./models/user");
 require("./models/post"); 
 app.use(express.json())
-app.use(require('./routes/authen.js'))
-app.use(require('./routes/post.js'))
+app.use(require('./routes/authen'))
+app.use(require('./routes/post'))
+app.use(require('./routes/user'))
 
 app.listen(PORT,()=>{
     console.log("running at ", PORT)
