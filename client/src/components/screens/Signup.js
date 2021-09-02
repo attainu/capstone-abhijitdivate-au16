6370501ce62abaa5fa63f9bac7dcfc4d9c3a0666
satender kumar <satenderkumarparihar@gmail.com>
2021-09-02 10:39:45 +0530

    Signup page FE

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import M from "materialize-css"
 export const Signup = () =>{
+
     const history = useHistory()
     const[name, setname] = useState()
     const[email, setemail] = useState("")
     const[password, setpassword] = useState("")
-    const PostData = ()=>{
+    const[url,seturl] = useState(undefined)
+    const [image,setImage] = useState("")
+    useEffect(()=>{
+        if(url){
+            uploadFields()
+        }
+    },[url])
+
+    const uploadpic=()=>{
+        const data = new FormData()
+        data.append("file",image)
+        data.append("upload_preset","socialnetwork")
+        data.append("cloud_name","dfvaav8sj")
+        fetch("https://api.cloudinary.com/v1_1/dfvaav8sj/image/upload",{
+            method:"post",
+            body:data
+        }).then(res=>res.json())
+        .then(data=>{
+            seturl(data.url)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+
+    const uploadFields= ()=>{
         if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
             M.toast({html:"Inavlid Email ID",classes:"#c62828 red darken-3"})
             return
@@ -20,7 +45,8 @@ export const Signup = () =>{
                 {
                     name:name,
                     password:password,
-                    email:email
+                    email:email,
+                    pic:url
                 }
             )
         }).then(res=>res.json()).then(data=>{
@@ -33,7 +59,20 @@ export const Signup = () =>{
         }).catch(err=>{
             console.log(err)
         })
+
     }
+
+
+
+    const PostData = ()=>{
+        if(image){
+            uploadpic()
+        }else{
+            uploadFields()
+
+        }
+    }
+
     return(
         <div className="mycard">
         <div className="card auth-card">
@@ -62,6 +101,22 @@ export const Signup = () =>{
                 setpassword(e.target.value)
             }}
             />
+            <div class="file-field input-field">
+               <div className="btn waves-effect waves-light #e57373 red lighten-2">
+                <span>Upload photo</span>
+                <input type="file"
+                    onChange={(e)=>{
+                        
+                        setImage(e.target.files[0])
+                    }}
+                />
+            </div>
+            <div className="file-path-wrapper">
+                <input className="file-path validate" type="text"/>
+            </div>
+            </div>
+
+
         <button
         onClick={()=>{
             PostData()
