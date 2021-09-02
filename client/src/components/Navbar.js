a24a7203303f1f3f2447b367ed5b6dcedd43cdf0
satender kumar <satenderkumarparihar@gmail.com>
2021-09-02 10:43:35 +0530

    Navbar pafe FE updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,45 @@
-import React from "react"
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react"
+import { Link, useHistory } from 'react-router-dom';
+import { UserContext } from "../App";
 
 const Navbar=()=>{
+  const{state,dispatch}= useContext(UserContext)
+  const history= useHistory()
+  const renderList = ()=>{
+
+    if(state){
+      return [ 
+      <>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/create">Create Post</Link></li>
+        <li><Link to="/myfollowerspost">My following post</Link></li>
+        <li>
+        <button class="btn waves-effect waves-light #e57373  lighten-2" type="submit" name="action"
+        onClick={()=>{
+          localStorage.clear()
+          dispatch({type:"CLEAR"})
+          history.push("/signin")
+        }}>Logout
+          <i class="material-icons" >send</i>
+        </button>
+        </li>
+      </>
+      ]
+    }else{
+      return [ 
+      <>
+        <li><Link to="/signin">Signin</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+      </>
+      ]
+    }
+  }
     return(
         <nav>
         <div class="nav-wrapper">  
-          <Link to="/" className="brand-logo left b">The Social Network</Link>
+          <Link to={state?"/":"signin"} className="brand-logo left b">The Social Network</Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><Link to="/signin">Signin</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/create">Create Post</Link></li>
+            {renderList()}
           </ul>
         </div>
       </nav>
